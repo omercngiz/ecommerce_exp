c1118c15a8630ef14245128eff9af45d2ca7a650
Omer Cengiz <omer.cengiz-34@hotmail.com>
2026-01-07 23:27:09 +0300

    add product module

---
+++ product.js
@@ -0,0 +1,17 @@
+import { generateId } from "./utils.js";
+
+export default class Product {
+  constructor(name, description, price, stock) {
+    this.id = generateId();
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.stock = stock;
+    this.categories = [];
+  }
+
+  addCategory(category) {
+    this.categories.push(category);
+    category.products.push(this);
+  }
+} 

c3bf975dc10dd78976ac6aa49bbf63c7efb8bfc8
Omer Cengiz <omer.cengiz-34@hotmail.com>
2026-01-10 11:05:19 +0300

    run express app with routes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 
-import User from "./user.js";
-import Category from "./category.js";
-import Product from "./product.js";
+import User from "./models/user.js";
+import Category from "./models/category.js";
+import Product from "./models/product.js";
 import Database from "./database.js";
 
 const app = express();
@@ -13,36 +13,26 @@ app.get("/", (req, res) => {
 
 const db = new Database();
 
-const omer = new User("Omer", "Kara", "omerkara", "omerkara@gmail.com", "hashed_password");
-const ali = new User("Ali", "Veli", "aliveli", "aliveli@gmail.com", "hashed_password");
-const kerem = new User("Kerem", "Demir", "keremdemir", "keremdemir@gmail.com", "hashed_password");
-
-let users = [omer, ali, kerem];
-db.saveUser(users);
-
-const laptop = new Product("Laptop", "A high performance laptop", 1500, 10);
-const smartphone = new Product("Smartphone", "A latest model smartphone", 800, 25);
-const headphones = new Product("Headphones", "Noise cancelling headphones", 200, 50);
-
-let products = [laptop, smartphone, headphones];
-db.saveProduct(products);
-
-const electronics = new Category("Electronics", "Electronic devices and gadgets");
-const appliances = new Category("Appliances", "Home and kitchen appliances");
-
-let categories = [electronics, appliances];
-db.saveCategory(categories);
-
-electronics.addProduct(laptop);
-electronics.addProduct(smartphone);
-
-headphones.addCategory(electronics);
-
-omer.addToBasket(laptop, 1);
-ali.addToBasket(smartphone, 2);
-
-app.get("/users", (req, res) => {
+app.get("/users/all", (req, res) => {
   res.send(db.loadUser());
+});
+
+app.get("/users/:id", (req, res) => {
+  const userId = parseInt(req.params.id, 10);
+  const user = db.loadUser().find((u) => u.id === userId);
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404).send({ error: "User not found" });
+  }
+});
+
+app.get("/products/all", (req, res) => {
+  res.send(db.loadProduct());
+});
+
+app.get("/categories/all", (req, res) => {
+  res.send(db.loadCategory());
 });
 
 app.listen(3000, () => {

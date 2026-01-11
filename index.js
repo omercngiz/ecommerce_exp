691295eb27516c76c6035f116ab895a029ead826
Omer Cengiz <omer.cengiz-34@hotmail.com>
2026-01-11 17:37:25 +0300

    setup pug pages

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 import express from "express";
-
-import User from "./models/user.js";
-import Category from "./models/category.js";
-import Product from "./models/product.js";
-import Database from "./src/database/database.js";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 import userRouter from "./src/routes/user-routes.js";
 import categoryRouter from "./src/routes/category-routes.js";
 import productRouter from "./src/routes/product-routes.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
+
+// View engine setup
+app.set('views', join(__dirname, 'src', 'views'));
+app.set('view engine', 'pug');
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the E-commerce API");
+  res.render('index');
 });
+app.use("/user", userRouter);
+app.use("/category", categoryRouter);
+app.use("/product", productRouter);
 
-const db = new Database();
-
-app.get("/user", userRouter);
-app.get("/category", categoryRouter);
-app.get("/product", productRouter);
-
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log("E-commerce API is running on http://localhost:3000");
 });

aab32eef8521623507ecc7a58b20a0f3ef296869
Omer Cengiz <omer.cengiz-34@hotmail.com>
2026-01-11 22:11:44 +0300

    build raw UI: home about contact login register

--- index.js
+++ index.js
@@ -18,8 +18,29 @@ app.set('view engine', 'pug');
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.render('index');
+  res.render('home');
 });
+
+app.get("/home", (req, res) => {
+  res.render('home');
+});
+
+app.get("/about", (req, res) => {
+  res.render('about');
+});
+
+app.get("/contact", (req, res) => {
+  res.render('contact');
+});
+
+app.get("/login", (req, res) => {
+  res.render('login');
+});
+
+app.get("/register", (req, res) => {
+  res.render('register');
+});
+
 app.use("/user", userRouter);
 app.use("/category", categoryRouter);
 app.use("/product", productRouter);

e19f73e5a3c0be8a968a01fc40e6b85aaf6c82a9
Omer Cengiz <omer.cengiz-34@hotmail.com>
2026-01-29 00:22:38 +0300

    refactor file organization

--- database/index.js
+++ database/index.js
@@ -4,86 +4,43 @@
  * JSON Database Daemon Entry Point
  * 
  * Responsibilities:
- *  - Load environment configurations.
- *  - Initialize TCP server.
- *  - Start listening on configured port.
- *  - Terminate process on fatal startup errors.
+ *  - Create TCP server instance.
+ *  - Register global error handler, shutdown handler and server error handler.
+ *  - Initialize server listening on configured port.
  * 
  * This file represents the main runtime boundary of the application.
  * 
  * @module index
  * @author Ömer Cengiz
  * @license MIT
- * @version 1.0.0
+ * @version 2.0.0
  * 
  * Required Environment Variables:
  *  - PORT {number} - TCP server listening port.
  */
 
+import { createServer } from './connection/tcp-connection.js';
+import TCPDataHandler from './protocol/tcp-data-handler.js';
+import EnvConfig from './env-config.js';
+import { initServer } from './connection/init-server.js';
 
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import globalErrorHandler from './global-handlers/global-error-handler.js';
+import serverErrorHandler from './global-handlers/server-error-handler.js';
+import shutdownHandler from './global-handlers/shutdown-handler.js';
 
-/** @type{import("net").Server} */
-import server from './tcp-connection.js';
+/**
+ * TCP Server Instance
+ * @type {import("net").Server}
+ */
+const server = createServer(new TCPDataHandler());
+globalErrorHandler(server);
+shutdownHandler(server);
+serverErrorHandler(server);
 
 /**
  * Server Port
  * @type {number}
- * @default
+ * @default 51234
  */
-let PORT;
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-/**
- * Initializes environment variables from .env file.
- * Fatal startup errors cause immediate process termination.
- * This entry point does not attempt recovery.
- * 
- * @throws {Error} - If PORT is not defined.
- * @returns {number} - Parsed PORT number.
- */
-const loadConfig = () => {
-  dotenv.config({ path: path.join(__dirname, '.env') });
-  
-  let rawPORT = process.env.PORT;
-  if (!rawPORT) {
-    throw new Error('PORT is not defined in environment variables.');
-    }
-
-  const portNumber = parseInt(rawPORT, 10);
-  if (isNaN(portNumber) || portNumber <= 0 || portNumber > 65535) {
-    throw new Error(`Invalid PORT: ${rawPORT}`);
-    process.exit(1);
-  }
-  return portNumber;
-};
-
-/**
- * Starts TCP server and begins listening on the specified port.
- * This entry point does not attempt recovery.
- * 
- * @param {number} PORT - Port number to listen on.
- * @listens net.Server#listening
- * @throws {Error} - If server fails to start listening
- */
-const initServer = (PORT) => {
-  try {
-    server.listen(PORT, () => {
-      console.log(`✅ TCP Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    if(error instanceof Error) {
-      console.error('Fatal error starting TCP server:', error.message);
-    }else {
-      console.error('Unknown fatal error starting TCP server:', error);
-    }
-    process.exit(1);
-  }
-};
-
-PORT = loadConfig();
-initServer(PORT);
+let PORT = EnvConfig.get_port();
+initServer(server, PORT);

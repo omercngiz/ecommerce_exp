04b90b6e27119fa9009f920bc9242d184c010b44
Omer Cengiz <omer.cengiz-34@hotmail.com>
2026-01-30 14:38:38 +0300

    add error module

---
+++ database/error_module/custom-error.js
@@ -0,0 +1,9 @@
+/**
+ * Custom Error Module
+ * Defines a custom error class for database operations.
+ * @module database/error_module/custom-error
+ */
+
+export default class CustomError extends Error {
+    
+}

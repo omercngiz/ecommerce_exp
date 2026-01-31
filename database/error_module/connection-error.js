47dc8d7779a34ab7d4c40c4540539a906a386aca
Omer Cengiz <omer.cengiz-34@hotmail.com>
2026-01-31 16:05:10 +0300

    add connection error module

---
+++ database/error_module/connection-error.js
@@ -0,0 +1,29 @@
+import BaseError from "./base-error.js";
+
+/**
+ * ConnectionError class for handling connection-related errors.
+ * 
+ * @extends {BaseError}
+ * @property {string} message - Error message.
+ * @property {string} code - Specific error code.
+ * @property {string} source - The code block or function where the error originated.
+ * @property {Object} [meta={}] - Additional metadata related to the error.
+ */
+export class ConnectionError extends BaseError {
+  /**
+   * @param {string} message
+   * @param {string} code
+   * @param {string} source
+   * @param {Object} [meta={}]
+   */
+  constructor(message, code, source, meta = {}) {
+    super({
+      message,
+      code: code,
+      layer: "connection",
+      source: source,
+      severity: BaseError.SEVERITY.WARNING,
+      meta,
+    });
+  }
+}

689f866e1bc0f5bfd4a378c0036b48dd04a7808a
Omer Cengiz <omer.cengiz-34@hotmail.com>
2026-01-31 18:53:49 +0300

    add mapping to constants

--- database/error_module/connection-error.js
+++ database/error_module/connection-error.js
@@ -4,7 +4,6 @@ import BaseError from "./base-error.js";
  * ConnectionError class for handling connection-related errors.
  * 
  * @extends {BaseError}
- * @property {string} message - Error message.
  * @property {string} code - Specific error code.
  * @property {string} source - The code block or function where the error originated.
  * @property {Object} [meta={}] - Additional metadata related to the error.
@@ -12,8 +11,8 @@ import BaseError from "./base-error.js";
 export class ConnectionError extends BaseError {
   static codes = Object.freeze({
     SOCKET_TIMEOUT: "CONNECTION_SOCKET_TIMEOUT",
-    REFUSED: "CONNECTION_REFUSED",
-    RESET: "CONNECTION_RESET",
+    CONNECTION_REFUSED: "CONNECTION_REFUSED",
+    CONNECTION_RESET: "CONNECTION_RESET",
     UNKNOWN_HOST: "CONNECTION_UNKNOWN_HOST",
     MAX_CONNECTION_REACHED: "CONNECTION_MAX_CONNECTION_REACHED",
     NO_RESPONSE_GENERATED: "CONNECTION_NO_RESPONSE_GENERATED",
@@ -21,20 +20,43 @@ export class ConnectionError extends BaseError {
 
   });
 
+  static severityMap = Object.freeze({
+    SOCKET_TIMEOUT: BaseError.SEVERITY.WARNING,
+    REFUSED: BaseError.SEVERITY.ERROR,
+    RESET: BaseError.SEVERITY.ERROR,
+    UNKNOWN_HOST: BaseError.SEVERITY.CRITICAL,
+    MAX_CONNECTION_REACHED: BaseError.SEVERITY.CRITICAL,
+    NO_RESPONSE_GENERATED: BaseError.SEVERITY.ERROR,
+    SOCKET_ERROR: BaseError.SEVERITY.ERROR,
+  });
+
+  static errorMessageMap = Object.freeze({
+    SOCKET_TIMEOUT: "The connection has timed out.",
+    CONNECTION_REFUSED: "The connection was refused by the server.",
+    CONNECTION_RESET: "The connection was reset.",
+    UNKNOWN_HOST: "The specified host is unknown.",
+    MAX_CONNECTION_REACHED: "The maximum number of connections has been reached.",
+    NO_RESPONSE_GENERATED: "No response was generated from the server.",
+    SOCKET_ERROR: "A socket error occurred during the connection.",
+  });
+
   /**
-   * @param {string} message
    * @param {string} code
    * @param {string} source
+   * @param {Error|null} cause
    * @param {Object} [meta={}]
    */
-  constructor(message, code, source, meta = {}) {
+  constructor(code, source, cause, meta = {}) {
     super({
-      message,
       code: code,
-      layer: "connection",
       source: source,
-      severity: BaseError.SEVERITY.WARNING,
-      meta,
+      cause: cause,
+      meta: meta,
+      message: ConnectionError.errorMessageMap[code],
+      layer: "connection",
+      severity: ConnectionError.severityMap[code],
     });
   }
 }
+
+// throw new ConnectionError(ConnectionError.codes.CONNECTION_REFUSED, "connection.tcp-connection", { host: "localhost", port: 5432 });

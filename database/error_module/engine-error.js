10ad0ba6c3bd1051e8ba5fe4159cefcd819d49c4
Omer Cengiz <omer.cengiz-34@hotmail.com>
2026-01-31 16:05:34 +0300

    add engine error module

---
+++ database/error_module/engine-error.js
@@ -0,0 +1,29 @@
+import BaseError from "./base-error.js";
+
+/**
+ * EngineError class for handling engine-related errors.
+ * 
+ * @extends {BaseError}
+ * @property {string} message - Error message.
+ * @property {string} code - Specific error code.
+ * @property {string} source - The code block or function where the error originated.
+ * @property {Object} [meta={}] - Additional metadata related to the error.
+ */
+export class EngineError extends BaseError {
+  /**
+   * @param {string} message
+   * @param {string} code
+   * @param {string} source
+   * @param {Object} [meta={}]
+   */
+  constructor(message, code, source, meta = {}) {
+    super({
+      message,
+      code: code,
+      layer: "engine",
+      source: source,
+      severity: BaseError.SEVERITY.WARNING,
+      meta,
+    });
+  }
+}

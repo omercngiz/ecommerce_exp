c98999a01278845ab11a731716bccde9c68b530d
Omer Cengiz <omer.cengiz-34@hotmail.com>
2026-01-31 18:38:45 +0300

    add error policy module

---
+++ database/error_module/error-policy.js
@@ -0,0 +1,9 @@
+/**
+ * Defines error formats.
+ * 2 formats:
+ *  - Server Error Format
+ *  - Client Error Format
+ * 
+ * Server Error format is used for logging and internal tracking.
+ * Client Error format is used for sending error responses to clients.
+ */

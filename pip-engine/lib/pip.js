ef101a6b3c2bf3419e2f24fb9b6dc8ace00723b4
Omer Cengiz <omer.cengiz-34@hotmail.com>
2026-02-06 00:44:47 +0300

    add new version

--- pip-engine/lib/pip.js
+++ pip-engine/lib/pip.js
@@ -3,6 +3,9 @@
  * 
  * @version 1.0.1
  *  - Streaming feature added. Now supports fragmented data reception over TCP. 05.01.2026
+ * 
+ * @version 1.0.2
+ *  - The parser has been made stateful for each socket connection. 06.01.2026 
  */
 
 require('./pip-server.js');

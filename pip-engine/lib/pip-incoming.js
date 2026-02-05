15a37a25b54b033a74f5b6b2c3592f3854060752
Omer Cengiz <omer.cengiz-34@hotmail.com>
2026-02-05 21:39:40 +0300

    remove jsdoc

--- pip-engine/lib/pip-incoming.js
+++ pip-engine/lib/pip-incoming.js
@@ -1,11 +1,3 @@
-/**
- * 
- * @version 1.0.0
- * 
- * @version 1.1.0
- *  - Streaming feature added. Now supports fragmented data reception over TCP. 05.01.2026
- */
-
 'use strict';
 
 const { REQUEST, RESPONSE } = require('./frame');
